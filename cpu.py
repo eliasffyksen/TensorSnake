755c96a2dd06998d4f662ee7be783523db409b62
Elias F. Fyksen <git@fyksen.net>
2022-08-06 10:31:15 +0100

    Refactor cuda code

---
+++ cpu.py
@@ -0,0 +1,16 @@
+from TensorSnake import TensorSnake
+import torch as t
+from timeit import timeit
+
+print('building')
+snake_count = 33 * 10 ** 6
+ts = TensorSnake(8, float_type=t.float64)
+ts = t.jit.script(ts)
+
+print('init...')
+ts.init(snake_count)
+print('running')
+
+for _ in range(5):
+  actions = t.randint(0,2,(snake_count,))
+  print(timeit(lambda: ts(actions), number=1))
